f83abb661c3387e5103588cf124e8f5fd805cbe2
niloofarsohrabi <nilufar.sohrabi@gmail.com>
2025-10-31 22:15:42 +0330

    feat;Genrates teh mock data

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -2,16 +2,12 @@ import { v4 as uuidv4 } from 'uuid';
 import type { ListItem } from '../types/types';
 
 export const generateList = (count: number = 20): ListItem[] => {
-  const items: ListItem[] = [];
-
-  for (let i = 1; i <= count; i++) {
-    items.push({
+  return new Array(count).fill('').map((_, index) => {
+    return {
       id: uuidv4(),
-      date: Date.now() + i * 1000,
-      title: `Item ${i}`,
-      subtitle: `Subtitle Item ${i}`,
-    });
-  }
-
-  return items;
+      date: Date.now() + index * 1000,
+      title: `Item ${index}`,
+      subtitle: `Subtitle Item ${index}`,
+    };
+  });
 };
